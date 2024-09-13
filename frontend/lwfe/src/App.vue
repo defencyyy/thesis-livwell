057c2395b6e80f248ee2269ecfe429fbaed6a2ad
nenic <salangsang99@gmail.com>
2024-09-13 23:05:17 +0800

    adding menu bar and search bar

--- frontend/lwfe/src/App.vue
+++ frontend/lwfe/src/App.vue
@@ -17,12 +17,16 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
+  /* margin-top: 60px; */
 }
-
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
 </style>
--- frontend/lwfe/src/components/HelloWorld.vue
+++ frontend/lwfe/src/components/HelloWorld.vue
@@ -1,14 +1,71 @@
 <template>
+  <header>
+  <nav class="nav">
+      <div class="logo">
+        <img src="" alt="" id="poisonous" />
+        <h1>LIVWELL</h1>
+      </div>
+      <div>
+        <ul class="nav-links">
+          <!-- <li><a href="#about">Sign In</a></li>
+          <li><a href="#skills">Join</a></li>
+          <li><a href="#projects">Projects</a></li> -->
+          <li><a href="#SignIn">SignIn</a></li>
+          <li><a href="#join">Join</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+    <body>
+      <section id = "content">
+        <div class="searchbar">
+          <h1>Find the Perfect Apartment</h1>
+          <input type="type">
+          <button type="button">Search</button>
+          <div class="popular">
+            <h2>Popular:</h2>
+            <h3>Apartment</h3>
+            <h3>Condo</h3>
+            <button type="onclick">do</button>
+          </div>
+        </div>
+        <div class="sidepic">
+          <img src="https://t3.ftcdn.net/jpg/04/74/05/94/360_F_474059464_qldYuzxaUWEwNTtYBJ44VN89ARuFktHW.jpg" alt="">
+        </div>
+
+      </section>
+
+    </body>
+
+
+
+
+
+
+
+
+<!-- 
+
   <nav>
+    <div>
     <ul class = "navr">
-      <li class="navr">HOME</li>
+   
+      <li class="navr"><a href="">HOME</a></li>
       <li class="navr">ABOUT</li>
       <li class="navr">CONTACTS</li>
       <li class="navr">SWITCH</li>
       <li class="navr"> SIGN IN/OUT</li>
     </ul>
-  </nav>
+  </div>
+  </nav> -->
 
+
+
+
+
+
+<!-- 
   <div class="hello">
     <div>
       <p>Find The Perfect Apartment Space For Your School</p>
@@ -16,7 +73,7 @@
 
     <div>
       <p>picture</p>
-    </div>
+    </div> -->
     <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -44,7 +101,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul> -->
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -60,18 +117,98 @@ export default {
 
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Francois+One&display=swap");
 
 
-.navr {
+
+nav,
+.logo,
+.nav-links {
   display: flex;
-  justify-content: flex-end;
-  color: black;
-  padding-right: 30px;
-  padding-top: 10px;
+  align-items: center;
+  background-color: rgb(253, 204, 113);
  
+  
 }
 
-.hello
+body
+{
+  background-color: rgb(189, 189, 189);
+  width: 100%;
+  height: 100%;
+}
+.logo{
+  padding-right: 20%;
+  font-family: "Francois One", sans-serif;
+  font-size: larger;
+}
+nav {
+  justify-content: space-around;
+  height: 10vh;
+  margin-bottom: 0.5in;
+}
+
+.nav-links {
+  padding-left: 20%;
+  gap: 5rem;
+  list-style: none;
+  font-size: 1.5rem;
+}
+
+#content
+{
+  display: flex;
+  gap: 30rem;
+  justify-content: center;
+  padding-top: 3%;
+  
+}
+
+#content h1
+{
+  font-family: "Francois One", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 40px
+  
+}
+
+input
+{
+  width: 100%;
+  height: 10%;
+  border: none;
+
+}
+
+button
+{
+  background-color: green;
+  color: black;
+  border: none;
+  width: 25%;
+  height: 15%;
+  font-family: "Francois One", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: large;
+  cursor: pointer;
+}
+
+.popular
+{
+  display: flex;
+  padding-top: 20px;
+  gap: 2rem;
+}
+
+h3
+{
+  border: 1px solid rgb(0, 0, 0);
+  padding: 3px 3px 3px 3px;
+}
+
+/* .hello
 {
   display: flex;
 }
@@ -89,5 +226,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
