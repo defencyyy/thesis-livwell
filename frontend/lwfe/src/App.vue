c910f533c33527cdd1541973eef66c5e8f3c71fe
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-09-17 17:28:53 +0800

    Basics

--- frontend/lwfe/src/App.vue
+++ frontend/lwfe/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-
-  
-    <div>
-      <router-view></router-view>
+    <TheNavigation />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+            <div>
+              <router-view></router-view>
+            </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-
+import TheNavigation from './components/TheNavigation.vue';
 export default {
   name: 'App',
-  components: {},
+  components: {
+    TheNavigation,
+  },
   
 }
 </script>
--- frontend/lwfe/src/pages/UserRegister.vue
+++ frontend/lwfe/src/pages/UserRegister.vue
@@ -1,7 +1,27 @@
 <template>
-    <div>
-        <div>Register Page</div>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <div>
+        <div>
+            <h3>Register</h3>
+            <hr>
+        </div>
+        <form>
+            <div class="form-group">
+            <label >Email</label>
+            <input type="text" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label >Password</label>
+            <input type="password" class="form-control" >
+        </div>
+        <div class="my-3">
+            <button type="submit" class="btn btn-primary">Register</button>
+        </div>
+        </form>
     </div>
+    </div>
+</div>
 </template>
 <script>
 export default {};
