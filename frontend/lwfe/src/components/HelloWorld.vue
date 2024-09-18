e5fa38cd2d3216022c4626696888f163e207588d
nenic <salangsang99@gmail.com>
2024-09-19 00:40:40 +0800

    adding navbar using bootstrap

--- frontend/lwfe/src/components/HelloWorld.vue
+++ frontend/lwfe/src/components/HelloWorld.vue
@@ -1,40 +1,37 @@
 <template>
   <header>
-  <nav class="nav">
-      <div class="logo">
-        <img src="" alt="" id="poisonous" />
-        <h1>LIVWELL</h1>
-      </div>
-      <div>
-        <ul class="nav-links">
-          <!-- <li><a href="#about">Sign In</a></li>
-          <li><a href="#skills">Join</a></li>
-          <li><a href="#projects">Projects</a></li> -->
-          <li><a href="#SignIn">SignIn</a></li>
-          <li><a href="#join">Join</a></li>
+   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
+      <a href="" class="navbar-brand">LIVWELL</a>
+      
+      
+      
+      <button class="navbar-toggler" type="button" data-bs-toggle="collape" data-bs-targer="#navmenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+      <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item ">
+            <a href="" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">About</a>
+          </li>
         </ul>
       </div>
-    </nav>
+    </div>
+    
+   </nav>
   </header>
 
     <body>
-      <section id = "content">
-        <div class="searchbar">
-          <h1>Find the Perfect Apartment</h1>
-          <input type="type">
-          <button type="button">Search</button>
-          <div class="popular">
-            <h2>Popular:</h2>
-            <h3>Apartment</h3>
-            <h3>Condo</h3>
-            <button type="onclick">do</button>
-          </div>
-        </div>
-        <div class="sidepic">
-          <img src="https://t3.ftcdn.net/jpg/04/74/05/94/360_F_474059464_qldYuzxaUWEwNTtYBJ44VN89ARuFktHW.jpg" alt="">
-        </div>
-
-      </section>
+      
+      <div class="p-3 mb-2 bg-primary-subtle text-primary-emphasis" style="width: 100%; height: 100%;"></div>
 
     </body>
 
@@ -45,63 +42,7 @@
 
 
 
-<!-- 
 
-  <nav>
-    <div>
-    <ul class = "navr">
-   
-      <li class="navr"><a href="">HOME</a></li>
-      <li class="navr">ABOUT</li>
-      <li class="navr">CONTACTS</li>
-      <li class="navr">SWITCH</li>
-      <li class="navr"> SIGN IN/OUT</li>
-    </ul>
-  </div>
-  </nav> -->
-
-
-
-
-
-
-<!-- 
-  <div class="hello">
-    <div>
-      <p>Find The Perfect Apartment Space For Your School</p>
-    </div>
-
-    <div>
-      <p>picture</p>
-    </div> -->
-    <!-- <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul> -->
-  <!-- </div> -->
 </template>
 
 <script>
@@ -123,13 +64,20 @@ export default {
 
 nav,
 .logo,
-.nav-links {
-  display: flex;
-  align-items: center;
-  background-color: rgb(253, 204, 113);
- 
+.nav-links{
+  /* display: flex;
+  align-items: center; */
+  /* background-color: rgb(253, 204, 113); */
+  
   
 }
+
+
+
+
+
+
+
 
 body
 {
