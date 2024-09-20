35a814ab1efe607036d571539afa03a9c9ed2cf2
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-09-20 15:41:22 +0800

    attempting to connect to db

--- backend/lwbe/settings.py
+++ backend/lwbe/settings.py
@@ -40,12 +40,17 @@ INSTALLED_APPS = [
 
     # User
     'rest_framework',
-    'users',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
+    'accounts',
 ]
+CORS_ALLOWED_ORIGINS=["https://localhost:8080",]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
