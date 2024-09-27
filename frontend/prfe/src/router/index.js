122607168cf62fc02a0eebada8a60d363ae73e8e
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-09-27 17:08:51 +0800

    Update index.js

--- frontend/prfe/src/router/index.js
+++ frontend/prfe/src/router/index.js
@@ -4,7 +4,8 @@ import MainPage from '@/views/MainPage.vue';
 
 const routes = [
   { path: '/login', name: 'Login', component: BrokLogin },
-  { path: '/main', name: 'Main', component: MainPage }
+  { path: '/main', name: 'Main', component: MainPage },
+  { path: '/', redirect: '/login' },
 ];
 
 
