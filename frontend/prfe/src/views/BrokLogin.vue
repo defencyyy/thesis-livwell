5cce9674647bfe01da2d68cc830cc84fd5bf4532
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-09-27 16:44:35 +0800

    established basic routers for the broker platform

---
+++ frontend/prfe/src/views/BrokLogin.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>Login Page</h2>
+        <form @submit.prevent="login">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" v-model="username" id="username" required />
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" v-model="password" id="password" required />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        login() {
+            // Temporary logic: You can add your backend API call here later
+            if (this.username && this.password) {
+                this.$router.push('/main'); // Route to main page
+            } else {
+                alert("Please fill in both fields.");
+            }
+        }
+    }
+};
+</script>
