db411232015c902be4b4542708cd713e97cf0910
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-10-02 14:30:21 +0800

    added passwords to brokers and developers

--- backend/brokers/models.py
+++ backend/brokers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from companies.models import Company
 from affiliations.models import Affiliation
+from django.contrib.auth.hashers import make_password
 
 class Broker(models.Model):
   company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
@@ -11,6 +12,14 @@ class Broker(models.Model):
   contact_number = models.CharField(max_length=20)
   description = models.TextField(blank=True)
   picture = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  password = models.CharField(max_length=128, blank=True, null=True)
+
+
+  def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith('pbkdf2_'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+  
 
   def __str__(self):
     return self.name
