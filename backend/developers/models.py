c9a0d9b82f0a72beba6fd2560dd6beac054ce7eb
Cyrus <cysness@gmail.com>
2024-10-02 13:57:14 +0800

    Database: Pushed Migrations

--- backend/developers/models.py
+++ backend/developers/models.py
@@ -8,6 +8,7 @@ class Developer(models.Model):
   name = models.CharField(max_length=200)
   contact_number = models.CharField(max_length=20)
   description = models.TextField(blank=True)
+  picture = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
   
   def __str__(self):
     return self.name
