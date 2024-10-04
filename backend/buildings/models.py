3d8c0cbbb1b2f5a49d99e75cadf0df891d0cc57d
Cyrus <cysness@gmail.com>
2024-10-04 13:45:00 +0800

    Database: Added models for building

--- backend/buildings/models.py
+++ backend/buildings/models.py
@@ -3,7 +3,7 @@ from companies.models import Company
 
 class Building(models.Model):
   company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=100)
   description = models.TextField(blank=True)
   location = models.CharField(max_length=200)
   is_active = models.BooleanField(default=True)
