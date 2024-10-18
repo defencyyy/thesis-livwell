873388d475f07453b5b8eab54a9d8c8a2f1f0a40
Cyrus <cysness@gmail.com>
2024-10-19 01:43:29 +0800

    Revert "Merge pull request #19 from defencyyy/backend_quiambao"
    This reverts commit ce6fd49afad1f12eb89079d91721757949694cef, reversing
    changes made to 37bd9c2ff8784ceb5623ff89915935d34d341ee6.

--- backend/prbe/urls.py
+++ backend/prbe/urls.py
@@ -5,9 +5,4 @@ from . import view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', view.login_view, name='login'),
-    path('reset-password/', view.send_password_reset_email, name='reset-password'),#for brokers
-    path('brokpass/<int:uid>/<str:token>/', view.BrokResetPass, name='BrokResetPass'),#for brokers
-    path('devlogin/', view.login_view_dev, name='devlogin'),  # for developers
-    path('reset-dev-password/', view.send_dev_password_reset_email, name='reset-dev-password'),  # for developers
-    path('devpass/<int:uid>/<str:token>/', view.DevResetPass, name='DevResetPass'),  # for developers
 ]
--- frontend/prfe/src/router/index.js
+++ frontend/prfe/src/router/index.js
@@ -1,31 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BrokLogin from '@/views/BrokLogin.vue';
 import BrokMainPage from '@/views/BrokMainPage.vue';
 import DevMainPage from '@/views/DevMainPage.vue';
 import DevLogin from '@/views/DevLogin.vue';
-import BrokForgotPass from '@/views/BrokForgotPass.vue'; 
-import BrokResetPass from '@/views/BrokResetPass.vue';
-import DevForgotPass from '@/views/DevForgotPass.vue'; 
-import DevResetPass from '@/views/DevResetPass.vue';
 
 const routes = [
-    { path: '/broklogin', name: 'BrokLogin', component: BrokLogin },
-    { path: '/brokmain', name: 'BrokMain', component: BrokMainPage },
-    { path: '/devlogin', name: 'DevLogin', component: DevLogin },
-    { path: '/devmain', name: 'DevMain', component: DevMainPage },
-    { path: '/brokforgotpass', name: 'BrokForgotPass', component: BrokForgotPass },
-    //{ path: '/brokresetpass', name: 'BrokResetPass', component: BrokResetPass }, // Moved to the top
-    { path: '/brokresetpass/:uid/:token', name: 'BrokResetPass', component: BrokResetPass }, // Moved to the top
-    { path: '/devforgotpass', name: 'DevForgotPass', component: DevForgotPass },
-    { path: '/devresetpass/:uid/:token', name: 'DevResetPass', component: DevResetPass },
-    { path: '/', redirect: '/broklogin' },
+  { path: '/broklogin', name: 'BrokLogin', component: BrokLogin },
+  { path: '/brokmain', name: 'BrokMain', component: BrokMainPage },
+  { path: '/devlogin', name: 'DevLogin', component: DevLogin },
+  { path: '/devmain', name: 'DevMain', component: DevMainPage },
+  { path: '/', redirect: '/broklogin' },
 ];
 
+
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
 
-
-
-export default router;
+export default router
