5cc19976e8d6c51f77a97084f2cc3b7e9d51fea5
nenic <salangsang99@gmail.com>
2024-10-29 14:26:20 +0800

    "fixing the navbar of homepage, and adding landing page"

--- frontend/prfe/src/views/HomeView.vue
+++ frontend/prfe/src/views/HomeView.vue
@@ -1,14 +1,78 @@
 <template>
+ 
+ <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+    <div class="container">
+        <a class="navbar-brand" href="#!"><span style="color: black;">Liv</span><span style="color: #fff;">Well</span></a>
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto"> <!-- Center the nav items -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#!">Offer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#!">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#!">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#!">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#!">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav d-flex flex-row">
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="#!">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="#!">
+                      <img src="../assets/facebook.png" alt="facebook" style="width: 30px; height: 30px;">
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="#!">
+                      <img src="../assets/instagram.png" alt="instagram" style="width: 30px; height: 30px;">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+  <div class="d-flex justify-content-evenly bg-secondary">
+
+  <div class="home text-white">
+    <img alt="Vue logo" src="@/assets/logo.png"/>
+    
+    <h1>Welcome to LivWell!</h1>
+    <p>Explore listings or manage your properties with ease.</p>
+
+    <div class="button-container">
+      <button @click="goToDeveloperLogin">Developer Login</button>
+    
+    </div>
+  </div>
+
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
     <h1>Welcome to LivWell!</h1>
     <p>Explore listings or manage your properties with ease.</p>
 
     <div class="button-container">
-      <button @click="goToDeveloperLogin">Developer Login</button>
+
       <button @click="goToBrokerLogin">Broker Login</button>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -28,9 +92,15 @@ export default {
 <style scoped>
 .home {
   text-align: center;
-  margin-top: 50px;
+  margin-top: 120px;
+  margin-bottom: 100px;
+  
 }
 
+/* .homes
+{
+  display: flex;
+} */
 .button-container {
   margin-top: 20px;
 }
@@ -49,5 +119,9 @@ button {
 
 button:hover {
   background-color: #45a049;
+}
+
+nav{
+  background-color: #4caf50;
 }
 </style>
