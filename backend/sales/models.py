f8ba8f775780567e4226c2e16abb768d2a86b85d
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-10-31 11:54:56 +0800

    Trial

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -3,15 +3,15 @@ from units.models import Unit
 from customers.models import Customer
 from brokers.models import Broker
 from sites.models import Site
+from companies.models import Company
 
 class Sale(models.Model):
   site = models.ForeignKey(Site, on_delete=models.CASCADE)
   unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
   customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
   broker = models.ForeignKey(Broker, on_delete=models.CASCADE)
+  company = models.ForeignKey(Company, on_delete=models.CASCADE) 
   date_sold = models.DateField(auto_now_add=True)
-
-  # Make automatic
 
   def __str__(self):
     return f"Sale of {self.unit.title} to {self.customer.first_name} {self.customer.last_name}"
