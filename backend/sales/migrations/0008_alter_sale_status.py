e7af8e052126cf5b29c6c3725f8ee9d618b1cc03
Cyrus <cysness@gmail.com>
2024-11-22 19:40:53 +0800

    Revert "Merge remote-tracking branch 'origin/nenic' into backend_florendo"
    This reverts commit 72e6a30b0e66835621594488321a3d59986dd18c, reversing
    changes made to 7c5dc5e2985ea130a18499cf919042d3bb73d870.

--- backend/sales/migrations/0008_alter_sale_status.py
+++ backend/sales/migrations/0008_alter_sale_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sales', '0006_sale_payment_method_sale_payment_reference_and_more'),
+        ('sales', '0007_merge_20241115_0934'),
     ]
 
     operations = [
