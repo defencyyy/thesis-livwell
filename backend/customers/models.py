49608137c92701a7ddb5fd2ee6d44797c3721a35
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-11-29 19:40:27 +0800

    Polished customer codes

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from brokers.models import Broker
 from companies.models import Company
-from django.db.models import Max
 
 class Customer(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE) 
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     broker = models.ForeignKey(Broker, on_delete=models.DO_NOTHING)
     email = models.EmailField(max_length=50)
     contact_number = models.CharField(max_length=20)
     last_name = models.CharField(max_length=200, null=True)
     first_name = models.CharField(max_length=200, null=True)
-    
-    # Default value will be overridden in save method
-    customer_code = models.CharField(max_length=10, unique=True)
+
+    customer_code = models.CharField(max_length=10)  # Removed unique=True
+
+    class Meta:
+        unique_together = ('broker', 'customer_code')  # Added unique constraint
 
     @property
     def name(self):
@@ -21,20 +22,20 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.last_name}, {self.first_name}"
 
-    # Overriding the save method to automatically generate a unique customer code in the format CUST-001
     def save(self, *args, **kwargs):
         if not self.customer_code:
-            # Get the latest customer code number (if any)
-            last_code = Customer.objects.aggregate(Max('customer_code'))['customer_code__max']
+            last_code = (
+                Customer.objects.filter(broker=self.broker)
+                .order_by('-customer_code')
+                .first()
+            )
 
             if last_code:
-                # Extract the number part of the last code and increment it
-                last_num = int(last_code.split('-')[1])
-                new_code = f"CUST-{str(last_num + 1).zfill(3)}"  # Increment and zero-pad
+                last_num = int(last_code.customer_code.split('-')[1])
+                new_code = f"CUST-{str(last_num + 1).zfill(3)}"
             else:
-                # If no customers exist, start from 001
                 new_code = "CUST-001"
-            
-            self.customer_code = new_code  # Set the new unique customer code
+
+            self.customer_code = new_code
 
         super().save(*args, **kwargs)
