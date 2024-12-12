d7952ee804f21e0463d957123ffd73fa54009914
Nenic <94435256+arsenicsalangsang@users.noreply.github.com>
2024-12-12 21:59:53 +0800

    final of log in page

--- frontend/prfe/src/views/MainLogin.vue
+++ frontend/prfe/src/views/MainLogin.vue
@@ -1,63 +1,102 @@
 <template>
-  <section class="hero overlay">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-md-8 col-lg-6 col-xl-5"> <!-- Ensure responsive centering -->
-          <div class="card rounded-3 text-black">
-            <div class="card-body p-md-5 mx-md-4">
+  <section class="vh-100 vw-100 d-flex align-items-center justify-content-center">
+    <div class="container">
+      <div class="row gy-4 align-items-center">
+        <div class="col-12 col-md-6 col-xl-7">
+          <div class="d-flex justify-content-center ">
+            <div class="col-12 col-xl-9 text-start">
+              <img
+                class="img-fluid rounded mb-4"
+                loading="lazy"
+                width="245"
+                height="80"
+                alt="Logo of Company"
+              />
+              <hr class="border mb-4" />
+              <h2 class="h1 mb-4 text-black">
+                Welcome to Livwell
+              </h2>
+              <p class="lead mb-5">
+               Log in page
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-xl-5">
+          <div class="card border-0 rounded-4">
+            <div class="card-body p-3 p-md-4 p-xl-5">
+              <div class="row">
+                <div class="col-12">
+                  <div class="mb-4">
+                    <h2 class="h3">Log In</h2>
+                    <h3 class="fs-6 fw-normal text-secondary m-0">
+                      Welcome back! Please enter your details.
+                    </h3>
+                  </div>
+                </div>
+              </div>
               <form @submit.prevent="login">
-                <br />
-                <h2 class="text-start">Hello!</h2>
-                <p class="text-start underline">
-                  Welcome back! Please enter your details.
-                </p>
-                <br />
+                <div class="row gy-3 overflow-hidden">
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input
+                        type="text"
+                        id="username"
+                        v-model="username"
+                        class="form-control"
+                        placeholder="Username"
+                        required
+                      />
+                      <label for="username">Username</label>
+                    </div>
+                  </div>
 
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="username">Username</label>
-                  <input
-                    type="text"
-                    id="username"
-                    v-model="username"
-                    class="form-control"
-                    required
-                  />
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input
+                        type="password"
+                        id="password"
+                        v-model="password"
+                        class="form-control"
+                        placeholder="Password"
+                        required
+                      />
+                      <label for="password">Password</label>
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <div class="d-grid">
+                      <button
+                        class="btn btn-primary btn-lg"
+                        :disabled="loading"
+                        type="submit"
+                      >
+                        Log In
+                      </button>
+                    </div>
+                  </div>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="password">Password</label>
-                  <input
-                    type="password"
-                    id="password"
-                    v-model="password"
-                    class="form-control"
-                    required
-                  />
-                </div>
-
-                <div class="d-flex justify-content-between align-items-center mb-4">
-                  <router-link
-                    class="text-muted"
-                    to="/forgot-password"
-                    >Forgot Password</router-link
-                  >
-                  <router-link class="text-muted" to="/login"
-                    >Back to home</router-link
-                  >
-                </div>
-
-                <div class="text-center pt-1 mb-5 pb-1">
-                  <button
-                    class="btn btn-primary w-100 btn-block fa-lg gradient-custom-2 mb-3 pt-4 pb-4"
-                    :disabled="loading"
-                    type="submit"
-                  >
-                    Log In
-                  </button>
-                </div>
-
-                <p v-if="error" class="text-danger">{{ error }}</p>
+                <p v-if="error" class="text-danger mt-3">{{ error }}</p>
               </form>
+
+              <div class="row">
+                <div class="col-12">
+                  <div
+                    class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-4"
+                  >
+                    <router-link
+                      to="/forgot-password"
+                      class="link-secondary text-decoration-none"
+                    >
+                      Forgot Password
+                    </router-link>
+                  
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -86,7 +125,6 @@ export default {
 
       if (this.username && this.password) {
         try {
-          // Attempt to authenticate as a broker first
           let response = await fetch("http://localhost:8000/broker/login/", {
             method: "POST",
             headers: {
@@ -101,29 +139,30 @@ export default {
           const data = await response.json();
 
           if (response.ok && data.tokens) {
-            // Handle broker login
             this.handleLogin(data, "broker");
           } else {
-            // If broker login fails, try developer login
-            response = await fetch("http://localhost:8000/api/token/developer/", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                username: this.username,
-                password: this.password,
-              }),
-            });
+            response = await fetch(
+              "http://localhost:8000/api/token/developer/",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  username: this.username,
+                  password: this.password,
+                }),
+              }
+            );
 
             const datas = await response.json();
 
             if (response.ok && datas.tokens) {
-              // Handle developer login
               this.handleLogin(datas, "developer");
             } else {
               this.error =
-                data.message || "Login failed. Please check your credentials.";
+                data.message ||
+                "Login failed. Please check your credentials.";
             }
           }
         } catch (error) {
@@ -138,51 +177,43 @@ export default {
       }
     },
 
-    // Factor out the common login handling logic
     handleLogin(data, role) {
-      // Store JWT tokens in localStorage
       localStorage.setItem("accessToken", data.tokens.access);
       localStorage.setItem("refreshToken", data.tokens.refresh);
 
-      // Assuming the response contains user details and company info
       const user = {
         id: data.user.id,
         user_role: data.user.user_role,
         company_id: data.user.company_id,
-        company: data.user.company, // Assuming the company data is included in the response
+        company: data.user.company,
       };
 
-      // Dispatch login action to Vuex store
       this.$store.dispatch("login", {
         user: user,
         token: data.tokens.access,
       });
 
-      // Redirect to the corresponding dashboard
       this.$router.push(`/${role}/dashboard`);
-    }
+    },
   },
 };
 </script>
 
-
 <style>
-.overlay {
-  background: linear-gradient(to bottom, rgba(113, 113, 113, 0.4), rgba(44, 171, 255, 0.7));
+section
+{
+  background-color: rgb(41, 176, 254);
 }
-.hero {
-  background-size: cover;
-  background-repeat: no-repeat;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+.bg-primary {
+  background-color: #007bff;
+  color: white;
 }
 .card {
-  width: 100%; /* Makes card fit the container */
-  max-width: 500px; /* Limits the maximum width */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 .form-control {
-  font-size: 1rem; /* Adjust font size for inputs */
+  font-size: 1rem;
+  padding: 1rem;
 }
 </style>
