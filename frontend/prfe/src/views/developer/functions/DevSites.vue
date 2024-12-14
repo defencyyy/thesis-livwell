1aa7cdd3e0fee36db8aa619cd4a5f9d819724a1c
jeweljaime <jewelvaidaajaime@gmail.com>
2024-12-14 09:10:56 +0800

    company site management button

--- frontend/prfe/src/views/developer/functions/DevSites.vue
+++ frontend/prfe/src/views/developer/functions/DevSites.vue
@@ -107,50 +107,46 @@
                     </td>
 
                     <td>
-                      <!-- Three Dots Icon for each row -->
-                      <button
-                        class="btn btn-link"
-                        type="button"
-                        @click.stop="toggleDropdown(site)"
-                        style="
-                          border: none;
-                          background-color: transparent;
-                          color: #343a40;
-                          cursor: pointer;
-                          font-size: 18px;
-                        "
-                      >
-                        <i class="fas fa-ellipsis-h"></i>
-                        <!-- Horizontal Three Dots Icon -->
-                      </button>
-
-                      <!-- Dropdown Menu -->
-                      <div
-                        v-if="isDropdownVisible(site)"
-                        class="dropdown-menu show"
-                        style="position: absolute; right: 0"
-                      >
-                        <a
-                          class="dropdown-item"
-                          href="#"
-                          @click.stop="openEditModal(site)"
-                          >Edit</a
+                      <div class="broker-actions d-flex gap-2">
+                        <button
+                          @click="openEditModal(site)"
+                          style="
+                            border: none;
+                            background-color: transparent;
+                            color: #343a40;
+                            cursor: pointer;
+                            font-size: 18px;
+                          "
                         >
-
-                        <a
-                          class="dropdown-item"
+                          <i class="fas fa-edit"></i>
+                        </button>
+                        <button
                           v-if="!site.archived"
-                          href="#"
-                          @click.stop="archiveSite(site)"
-                          >Archive</a
+                          @click="archiveSite(site)"
+                          style="
+                            border: none;
+                            background-color: transparent;
+                            color: #343a40;
+                            cursor: pointer;
+                            font-size: 18px;
+                          "
                         >
-                        <a
-                          class="dropdown-item"
+                          <i class="fas fa-archive"></i>
+                        </button>
+                        <button
                           v-else
-                          href="#"
-                          @click.stop="unarchiveSite(site)"
-                          >Unarchive</a
+                          @click="unarchiveSite(site)"
+                          class="btn btn-sm btn-success"
+                          style="
+                            border: none;
+                            background-color: transparent;
+                            color: #343a40;
+                            cursor: pointer;
+                            font-size: 18px;
+                          "
                         >
+                          <i class="fas fa-undo"></i>
+                        </button>
                       </div>
                     </td>
                   </tr>
