480b1f6e588b27a6bc2e0be08219858b56b5bf9f
Cyrus <cysness@gmail.com>
2024-12-22 23:18:43 +0800

    Update settings.py

--- backend/prbe/settings.py
+++ backend/prbe/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '13.215.50.86', 'ec2-13-215-50-86.ap-southeast-1.compute.amazonaws.com']
 
 # Application definition
 
