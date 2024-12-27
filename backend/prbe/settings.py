c8a27251770fe20d7cb0c7d639f634f29b7d015e
Cyrus <cysness@gmail.com>
2024-12-28 00:41:42 +0800

    Update settings.py

--- backend/prbe/settings.py
+++ backend/prbe/settings.py
@@ -161,10 +161,6 @@ STATIC_URL = 'static/'
 # Directory to collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Additional directories for static file lookup
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
